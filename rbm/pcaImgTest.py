c464f6592b0f08b7635587a4a28ab5070d18383c
Jason Yosinski <jason@yosinski.com>
2012-04-22 22:28:40 -0400

    Finished pcaImgTest!

--- rbm/pcaImgTest.py
+++ rbm/pcaImgTest.py
@@ -10,15 +10,7 @@ from pca import PCA
 from upsonRbm import loadUpsonData
 from ResultsManager import resman
 
-#import matplotlib
-#matplotlib.use('Agg') # plot with no display
 from matplotlib import pyplot
-
-#from matplotlib import rc
-#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],'size':8})
-## for Palatino and other serif fonts use:
-#rc('font',**{'family':'serif','serif':['Palatino']))
-#rc('text', usetex=True)
 
 
 
@@ -47,37 +39,34 @@ def main():
     pyplot.savefig(os.path.join(resman.rundir, 'varstd.png'))
     pyplot.close()
 
-
     #font = ImageFont.truetype('/usr/share/fonts/truetype/ttf-lyx/cmr10.ttf', 10)
     font = ImageFont.truetype('/usr/share/texmf/fonts/opentype/public/lm/lmmono12-regular.otf', 14)
-    for dims in [1, 2, 5, 10, 20, 50, 100, 200, 225]:
-        for ee, epsilon in enumerate([0, 1e-4, 1e-3, 1e-2, 1e-1, 1]):
-            arr = tile_raster_images(
-                     X = pca.zca(train_set_x_data, dims, epsilon = epsilon),
-                     img_shape = (img_dim,img_dim),tile_shape = (10,10),
-                     tile_spacing=(1,1))
 
-            arrHeight = arr.shape[0]
+    def plotImage(xx, filename, str = None):
+        arr = tile_raster_images(X = xx,
+                                 img_shape = (img_dim,img_dim),tile_shape = (10,10),
+                                 tile_spacing=(1,1))
+        arrHeight = arr.shape[0]
+        if str is not None:
             arr = vstack((arr, zeros((20, arr.shape[1]), dtype = arr.dtype)))
-            image = Image.fromarray(arr)
+        image = Image.fromarray(arr)
+        if str is not None:
             draw = ImageDraw.Draw(image)
-            draw.text((2, arrHeight+2), 'dims=%d, eps=%s' % (dims, repr(epsilon)), 255, font = font)
+            draw.text((2, arrHeight+2), str, 255, font = font)
             draw = ImageDraw.Draw(image)
-            image.save(os.path.join(resman.rundir, 'samplesZCA_%03d_%02d.png' % (dims, ee)))
+        image.save(os.path.join(resman.rundir, filename))
 
-            # HERE Plot just PCA
-            #arr = tile_raster_images(
-            #         X = pca.zca(train_set_x_data, dims, epsilon = epsilon),
-            #         img_shape = (img_dim,img_dim),tile_shape = (10,10),
-            #         tile_spacing=(1,1))
-            #
-            #arrHeight = arr.shape[0]
-            #arr = vstack((arr, zeros((20, arr.shape[1]), dtype = arr.dtype)))
-            #image = Image.fromarray(arr)
-            #draw = ImageDraw.Draw(image)
-            #draw.text((2, arrHeight+2), 'dims=%d, eps=%s' % (dims, repr(epsilon)), 255, font = font)
-            #draw = ImageDraw.Draw(image)
-            #image.save(os.path.join(resman.rundir, 'samplesZCA_%03d_%02d.png' % (dims, ee)))
+    plotImage(pca.pc().T, 'pc.png')
+
+    for dims in [1, 2, 5, 10, 20, 50, 100, 200, 225]:
+        plotImage(pca.pcaAndBack(train_set_x_data, dims),
+                  'samplesPCA_%03d.png' % dims,
+                  'dims=%d' % dims)
+
+        for ee, epsilon in enumerate([0, 1e-4, 1e-3, 1e-2, 1e-1, 1, 2, 5]):
+            plotImage(pca.toZca(train_set_x_data, dims, epsilon = epsilon),
+                      'samplesZCA_%03d_%02d.png' % (dims, ee),
+                      'dims=%d, eps=%s' % (dims, repr(epsilon)))
 
 
 
