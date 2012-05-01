6bcf7f06e0356fb632926816cc0f940852308527
Jason Yosinski <jason@yosinski.com>
2012-05-01 18:58:32 -0400

    added circles to squaresRbm

--- rbm/squaresRbm.py
+++ rbm/squaresRbm.py
@@ -1,9 +1,7 @@
 #! /usr/bin/env python
 
-
-
 import numpy, time, gzip, PIL.Image, os, pdb
-import pickle
+import pickle, sys
 from numpy import *
 
 from ResultsManager import resman
@@ -12,7 +10,7 @@ from rbm import RBM, test_rbm
 
 
 
-def loadSquaresData(trainFile, testFile):
+def loadPickledData(trainFile, testFile):
     ''' Loads the dataset and returns in the expected train,valid,test format.'''
 
     # Load the dataset
@@ -29,13 +27,26 @@ def loadSquaresData(trainFile, testFile):
 
 
 if __name__ == '__main__':
-    resman.start('junk', diary = False)
-    datasets = loadSquaresData('../data/squares/train_10_50000.pkl.gz',
-                               '../data/squares/test_10_50000.pkl.gz')
+    resman.start('junk', diary = True)
+
+    circles = False
+    if len(sys.argv) > 1 and sys.argv[1] == '--circles':
+        circles = True
+        del sys.argv[1]
+
+    print 'Using dataset:', 'circles' if circles else 'squares'
+
+    img_dim = 10    # 2, 4, 10, 15, 28
+    if circles:
+        datasets = loadPickledData('../data/circles/train_%d_50000.pkl.gz' % img_dim,
+                                   '../data/circles/test_%d_50000.pkl.gz' % img_dim)
+    else:
+        datasets = loadPickledData('../data/squares/train_%d_50000.pkl.gz' % img_dim,
+                                   '../data/squares/test_%d_50000.pkl.gz' % img_dim)
     print 'done loading.'
     test_rbm(datasets = datasets,
              training_epochs = 5,
-             img_dim = 10,
+             img_dim = img_dim,
              n_hidden = 100, 
              learning_rate = .1, 
              output_dir = resman.rundir,
