8833c2c7d1fc7d00533e8ebdb5847bbc3ce55db0
Jason Yosinski <jason@yosinski.com>
2012-05-02 14:47:45 -0400

    Updated makeCircles to save demo image

--- makeData/makeCircles.py
+++ makeData/makeCircles.py
@@ -5,9 +5,10 @@ import os, pdb, gzip
 from PIL import Image
 from numpy import *
 import cPickle as pickle
+import numpy
 
 from matplotlib import pyplot
-from rbm.utils import imagesc
+from rbm.utils import imagesc, tile_raster_images
 from makeUpsonRovio1 import saveToFile
 
 
@@ -39,19 +40,35 @@ def randomCircleSampleMatrix(Nw = 10, Nsamples = 10):
 
 
 
-def main(demo = True):
-    if demo:
-        random.seed(0)
-        xx, yy = randomCircleSampleMatrix(Nw = 15, Nsamples = 25)
+def demo():
+    random.seed(0)
+    Nw = 15
+    xx, yy = randomCircleSampleMatrix(Nw = Nw, Nsamples = 100)
 
+
+    saveImage = False
+    if saveImage:
+        image_data = numpy.ones(((Nw+1)*10-1,(Nw+1)*10-1), dtype='uint8') * 51  # dark gray
+        tmp = tile_raster_images(X = xx,
+                                 img_shape = (Nw,Nw),
+                                 tile_shape = (10,10),
+                                 tile_spacing = (1,1))
+        image_data = tmp
+        image = Image.fromarray(image_data)
+        image.save(os.path.join('demoCircleSamples.png'))
+    else:
         pyplot.figure()
         for ii in range(25):
             ax = pyplot.subplot(5,5,ii)
-            imagesc(xx[ii,:].reshape((15,15)), ax=ax)
+            imagesc(xx[ii,:].reshape((Nw,Nw)), ax=ax)
             #pyplot.title(repr(yy[ii,:]))
             pyplot.title('%d, %d, %.2f' % tuple(yy[ii,:]))
         pyplot.show()
 
+
+
+
+def main():
     random.seed(0)
 
     saveToFile('../data/circles/train_02_50.pkl.gz',    randomCircleSampleMatrix(Nw = 02, Nsamples = 50))
@@ -82,5 +99,6 @@ def main(demo = True):
 
 
 if __name__ == '__main__':
+    demo()
     #main()
-    main(demo = False)
+
