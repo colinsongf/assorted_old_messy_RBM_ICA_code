ccdb533bb761b8a30fd54ab524ead3992eeefa20
Jason Yosinski <jason@yosinski.com>
2012-05-08 17:09:20 -0400

    seems to work, time to train everything

--- rbm/generativeLearn.py
+++ rbm/generativeLearn.py
@@ -36,7 +36,6 @@ def makeSizedDataset(size, cubeDatasets, sphereDatasets, appendClass):
 
 
 def main():
-    
     # Load both squares and spheres datasets
     img_dim = 10    # 2, 4, 10, 15, 28
     cubeDatasets = loadPickledData('../data/cubes/train_%d_50000.pkl.gz' % img_dim,
@@ -44,7 +43,7 @@ def main():
     sphereDatasets = loadPickledData('../data/spheres/train_%d_50000.pkl.gz' % img_dim,
                                      '../data/spheres/test_%d_50000.pkl.gz' % img_dim)
 
-    # reduce to 20000 rows to prevent memory error
+    # If necessary, reduce to 20000 rows to prevent memory error
     #cubeDatasets = ((cubeDatasets[0][0][:20000,:], None),
     #                (None, None),
     #                (cubeDatasets[2][0][:20000,:], None))
@@ -53,41 +52,42 @@ def main():
     #                  (sphereDatasets[2][0][:20000,:], None))
 
     # make different size datasets
-    #sizes = [10, 20, 40, 100, 200, 400, 1000, 2000, 4000, 10000, 20000, 40000]
-    sizes = [10, 20]
+    sizes = [10, 20, 40, 100, 200, 400, 1000, 2000, 4000, 10000, 20000, 40000]
+    #sizes = [10, 20]
 
     sizedDatasetsX = {}
     sizedDatasetsXY = {}
     for size in sizes:
         sizedDatasetsX[size]  = makeSizedDataset(size, cubeDatasets, sphereDatasets, appendClass = False)
-        sizedDatasetsXY[size] = makeSizedDataset(size, cubeDatasets, sphereDatasets, appendClass = False)
-    testDataset = makeSizedDataset(40000, cubeDatasets, sphereDatasets, appendClass = False)
+        sizedDatasetsXY[size] = makeSizedDataset(size, cubeDatasets, sphereDatasets, appendClass = True)
+    testDatasetX  = makeSizedDataset(40000, cubeDatasets, sphereDatasets, appendClass = False)
+    testDatasetXY = makeSizedDataset(40000, cubeDatasets, sphereDatasets, appendClass = True)
 
-    #xDatasets  = makeSizedDatasets(sizes, cubeDatasets, sphereDatasets, appendClass = False)
-    #xyDatasets = makeSizedDatasets(sizes, cubeDatasets, sphereDatasets, appendClass = True)
-    #xDatasets  = makeSizedDatasets(sizes, cubeX, sphereX, appendClass = False)
-    #xyDatasets = makeSizedDatasets(sizes, cubeX, sphereX, appendClass = True)
-    
-    # train something...
     print 'done loading.'
 
-    for size in sizes:
-        print 'Size:', size
-        thisDir = os.path.join(resman.rundir, 'size_%05d' % size)
-        os.mkdir(thisDir)
-        thisDataset = (sizedDatasetsX[size], (array([]), None), testDataset)
-        rbm, meanCosts = test_rbm(datasets = thisDataset,
-                                  training_epochs = 1,
-                                  img_dim = img_dim,
-                                  n_hidden = 200, 
-                                  learning_rate = .1, 
-                                  output_dir = thisDir,
-                                  quickHack = False,
-                                  imgPlotFunction = lambda xx: xx[:,0:img_dim*img_dim],  # HACK: plot first slice
-                                  )
+    for useXY in [False, True]:
+        for size in sizes:
+            print 'useXY', useXY, ', Size:', size
+            thisDir = os.path.join(resman.rundir, '%s_size_%05d' % ('xy' if useXY else 'x', size))
+            os.mkdir(thisDir)
+            if useXY:
+                thisDataset = (sizedDatasetsXY[size], (array([]), None), testDatasetXY)
+            else:
+                thisDataset = (sizedDatasetsX[size],  (array([]), None), testDatasetX)
+
+            # this automatically saves the RBM to the given directory
+            rbm, meanCosts = test_rbm(datasets = thisDataset,
+                                      training_epochs = 45,
+                                      img_dim = img_dim,
+                                      n_hidden = 200, 
+                                      learning_rate = .1, 
+                                      output_dir = thisDir,
+                                      quickHack = False,
+                                      imgPlotFunction = lambda xx: xx[:,0:img_dim*img_dim],  # HACK: plot first slice
+                                      )
 
 
 if __name__ == '__main__':
-    resman.start('junk', diary = False)
+    resman.start('junk', diary = True)
     main()
     resman.stop()
