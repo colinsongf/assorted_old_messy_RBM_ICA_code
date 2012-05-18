54a62bfd08e04fb49f880633e5041d11f1cbba8d
Jason Yosinski <jason@yosinski.com>
2012-05-18 01:50:53 -0400

    minor

--- ica/upsonIca.py
+++ ica/upsonIca.py
@@ -25,8 +25,10 @@ if __name__ == '__main__':
     '''Demonstrate ICA on the Upson data set.'''
 
     resman.start('junk', diary = False)
-    datasets = loadUpsonData('../data/upson_rovio_1/train_10_50000.pkl.gz',
-                             '../data/upson_rovio_1/test_10_50000.pkl.gz')
+
+    imgDim = 10    # 2, 4, 10, 15, 28
+    datasets = loadUpsonData('../data/upson_rovio_1/train_%d_50000.pkl.gz' % imgDim,
+                             '../data/upson_rovio_1/test_%d_50000.pkl.gz' % imgDim)
     testIca(datasets = datasets,
             savedir = resman.rundir,     # comment out to show plots instead of saving
             smallImgHack = False,
