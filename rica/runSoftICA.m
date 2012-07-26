16682c488bc8184baa2ca9b22b7bec2c623ad64b
Jason Yosinski <jason@yosinski.com>
2012-07-26 12:39:50 -0400

    Second pristine version of RICA from updated .zip file. Donwloaded 7/25/12.

--- rica/runSoftICA.m
+++ rica/runSoftICA.m
@@ -1,11 +1,12 @@
 %% Note: This sample code requires minFunc to run.
 %        But you can use softICACost.m with your own optimizer of choice.
 clear all;
-%addpath minFunc/   % this should point to minFunc
-addpath ~/work/RNN/minFunc/
+addpath ~/work/RNN/minFunc/   % this should point to minFunc
+                              % http://www.di.ens.fr/~mschmidt/Software/minFunc.html
+                              % minFunc 2009 seems to work well
 %% Load and configure a training dataset
 global params;
-params.m=50000;                 % num patches
+params.m=20000;                 % num patches
 params.patchWidth=16;           % width of a patch
 params.n=params.patchWidth^2;   % dimensionality of input
 
@@ -20,21 +21,21 @@ x = bsxfunwrap(@rdivide,patches,m);
 
 %% Run the optimization
 
-params.lambda = 0.005;
-params.numFeatures = 3200;
+params.lambda = 0.05;
+params.numFeatures = 400;
 params.epsilon = 1e-5;
 
 %configure minFunc
 options.Method = 'lbfgs';
 options.MaxFunEvals = Inf;
-options.MaxIter = 4000;
+options.MaxIter = 300;
 %options.display = 'off';
 %options.outputFcn = 'showBases';
 
 % initialize with random weights
-randTheta = randn(params.numFeatures*params.n,1)*0.01;  % 1/sqrt(params.n);
+randTheta = randn(params.numFeatures,params.n)*0.01;  % 1/sqrt(params.n);
 randTheta = randTheta ./ repmat(sqrt(sum(randTheta.^2,2)), 1, size(randTheta,2)); 
-
+randTheta = randTheta(:);
 
 % optimize
 [opttheta, cost, exitflag] = minFunc( @(theta) softICACost(theta, x, params), randTheta, options);   % Use x or xw 
