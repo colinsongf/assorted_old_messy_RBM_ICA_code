c0fed2154c0487d1bc84c894d9dca6cbae6b6544
Jason Yosinski <jason@yosinski.com>
2012-08-21 16:00:27 -0400

    Ready for some RICA runs

--- ica/rica.py
+++ ica/rica.py
@@ -13,10 +13,12 @@ from matplotlib import pyplot, mlab
 from PIL import Image
 
 from util.ResultsManager import resman
+from util.plotting import tile_raster_images
 from rbm.pca import PCA
-from rbm.utils import tile_raster_images, load_mnist_data, saveToFile, looser
+from rbm.utils import load_mnist_data, saveToFile, looser
 
-from scipy.optimize import fmin_bfgs
+#from scipy.optimize import fmin_bfgs, minimize
+from scipy.optimize.lbfgsb import fmin_l_bfgs_b
 from util.dataLoaders import loadFromPklGz
 
 
@@ -42,15 +44,45 @@ def l2RowScaledGrad(xx, yy, outerDeriv):
 
 
 
+class OneTwoFunction(object):
+    '''Allows a function to be called that returns a tuple. '''
+
+    def __init__(self, function):
+        self.function = function
+        self.nextCall = 1
+        self.result = None
+
+    def one(self, value):
+        if self.nextCall != 1:
+            raise Exception('Wrong call order; was expecting %d' % self.nextCall)
+        self.lastValue = value
+        self.result = self.function(value)
+        self.nextCall = 2
+        return self.result[0]
+
+    def two(self, value):
+        if self.nextCall != 2:
+            raise Exception('Wrong call order; was expecting %d' % self.nextCall)
+        if value != self.lastValue:
+            raise Exception('Called two with a different value.')
+        self.nextCall = 1
+        return self.result[1]
+
+
+
 class RICA(object):
-    def __init__(self, lambd = .005, nFeatures = 800, epsilon = 1e-5):
+    def __init__(self, imgDim, lambd = .005, nFeatures = 800, epsilon = 1e-5, saveDir = ''):
         self.lambd     = lambd
         self.nFeatures = nFeatures
         self.epsilon   = epsilon
+        self.saveDir   = saveDir
+        self.imgDim    = imgDim
 
 
     def cost(self, WW, data):
         nInputDim = data.shape[0]
+        if self.imgDim ** 2 != nInputDim:
+            raise Exception('Expected %d * %d = %d input, but got %d' % (self.imgDim, self.imgDim, self.imgDim**2, nInputDim))
 
         # NOTE: Flattening and reshaping is in C order in numpy but Fortran order in Matlab. This should not matter.
         WW = WW.reshape(self.nFeatures, nInputDim)
@@ -107,7 +139,7 @@ class RICA(object):
         return cost, grad
 
 
-    def run(self, data):
+    def run(self, data, maxFun = 300):
         '''data should be one data point per COLUMN! (different)'''
         nInputDim = data.shape[0]
 
@@ -117,15 +149,54 @@ class RICA(object):
         WW = WW.flatten()
 
         # Run optimization
-        xopt = fmin_bfgs(lambda WW : self.cost(WW, data), WW)
+        #xopt = fmin_bfgs(lambda WW : self.cost(WW, data), WW)
+        #function = OneTwoFunction(lambda WW : self.cost(WW, data))
 
+        #def costFn(xx):
+        #    print 'costFn'
+        #    return self.cost(xx, data)[0]
+        #def gradFn(xx):
+        #    print 'gradFn'
+        #    return self.cost(xx, data)[1]
+        
+        #xopt = fmin_bfgs(costFn,
+        #                 WW,
+        #                 fprime = gradFn,
+        #                 maxiter = 3)
+        #xopt = minimize(lambda WW : self.cost(WW, data),
+        #                WW,
+        #                method = 'BFGS',
+        #                jac = True,    # returned along with cost
+        #                options = {'maxiter': 3, 'disp': True},
+        #                )
+
+        print 'Starting optimization, maximum function calls = ', maxFun
+        xopt, fval, info = fmin_l_bfgs_b(lambda WW : self.cost(WW, data),
+                                         WW,
+                                         fprime = None,   # function call returns value and gradient
+                                         approx_grad = False,
+                                         iprint = 1,
+                                         maxfun = maxFun)
+        
         print 'OPT DONE'
-        pdb.set_trace()
+        WW = xopt.reshape(self.nFeatures, nInputDim)
+
+        image = Image.fromarray(tile_raster_images(
+            X = WW,
+            img_shape = (self.imgDim, self.imgDim), tile_shape = (24,33),
+            tile_spacing=(1,1)))
+        if self.saveDir:  image.save(os.path.join(self.saveDir, 'WW.png'))
+        #image.show()
 
 
 
 if __name__ == '__main__':
+    resman.start('prica_010', diary = True)
+
     data = loadFromPklGz('../data/rica_hyv_patches_16.pkl.gz')
-    rica = RICA()
-    rica.run(data)
+
+    rica = RICA(imgDim = 16,
+                saveDir = resman.rundir)
+    rica.run(data, maxFun = 10)
     
+    resman.stop()
