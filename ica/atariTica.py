39932cc8e84330fc2db2dc6666be8f44ea0d3628
Jason Yosinski <jason@yosinski.com>
2012-11-10 21:41:45 -0500

    atariTica ready for some random runs

--- ica/atariTica.py
+++ ica/atariTica.py
@@ -25,17 +25,17 @@ if __name__ == '__main__':
     data = loadFromPklGz('../data/atari/mspacmantrain_15_50000_3c.pkl.gz')
     data = data.T   # Make into one example per column
     
-    hiddenISize = 20
-    hiddenJSize = 20
-    lambd = .05
-    neighborhoodSize = 1
-    print '\nChosen TICA parameters'
+    #hiddenISize = 20
+    #hiddenJSize = 20
+    #lambd = .05
+    #neighborhoodSize = 1
+    #print '\nChosen TICA parameters'
     
-    #hiddenISize = random.randint(4, 25+1)
-    #hiddenJSize = random.randint(10, 30+1)
-    #lambd = .05 * 2 ** random.randint(-4, 4+1)
-    #neighborhoodSize = random.randint(1, 4+1)
-    #print '\nRandomly selected TICA parameters'
+    hiddenISize = random.randint(4, 25+1)
+    hiddenJSize = random.randint(10, 30+1)
+    lambd = .1 * 2 ** random.randint(-5, 5+1)
+    neighborhoodSize = random.uniform(.1,3)
+    print '\nRandomly selected TICA parameters'
 
     for key in ['hiddenISize', 'hiddenJSize', 'lambd', 'neighborhoodSize']:
         print '  %20s: %s' % (key, locals()[key])
@@ -43,10 +43,10 @@ if __name__ == '__main__':
     random.seed(0)
     tica = TICA(imgShape = (15, 15, 3),
                 hiddenLayerShape = (hiddenISize, hiddenJSize),
-                neighborhoodSize = neighborhoodSize,
+                neighborhoodParams = ('gaussian', neighborhoodSize, 0),
                 lambd = lambd,
                 epsilon = 1e-5,
                 saveDir = resman.rundir)
-    tica.run(data, plotEvery = 1, maxFun = 300, whiten = True)
+    tica.run(data, plotEvery = 5, maxFun = 300, whiten = True)
 
     resman.stop()
