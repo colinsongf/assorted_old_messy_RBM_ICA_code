37514434e4f34b05245722de6cec5707541703e5
Jason Yosinski <jason@yosinski.com>
2013-07-06 13:29:59 -0400

    minor

--- ica/visualize.py
+++ ica/visualize.py
@@ -136,7 +136,7 @@ def plotReshapedActivations(act, tileShape, embeddingShape, prefix, saveDir = No
     number = prod(tileShape)
     dat = act[:,:number].copy()
     dat -= dat.min()
-    dat /- dat.max()
+    dat /- dat.max()        HERE BUG
     plotImageData(dat, imgShape = embeddingShape, prefix = prefix, saveDir = saveDir, show = show)
 
 
